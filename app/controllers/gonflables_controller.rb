17ded6641ec659c35b13b8c713fe68894e5d86f4
nicolas belin <belin.nicolas89@gmail.com>
2024-12-02 14:54:11 +0100

    gonflables controller done

---
+++ app/controllers/gonflables_controller.rb
@@ -0,0 +1,2 @@
+class GonflablesController < ApplicationController
+end
---
+++ test/controllers/gonflables_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class GonflablesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
