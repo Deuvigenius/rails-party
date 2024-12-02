913a6211455d7c9745ffd4551a43c05e8f2e8f7b
nicolas belin <belin.nicolas89@gmail.com>
2024-12-02 15:38:32 +0100

    model rent done

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -1,7 +1,4 @@
 class Rent < ApplicationRecord
   belongs_to :user
   belongs_to :gonflable
-
-  validates :gonflable_id, :user_id, presence: true
-  validates :gonflable_id, uniqueness: { scope: :user_id }
 end
