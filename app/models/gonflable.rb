5bdd10ddb44c8a5930da6a0814ffc1fb4f70c68b
nicolas belin <belin.nicolas89@gmail.com>
2024-12-04 14:52:22 +0100

    paginate

--- app/models/gonflable.rb
+++ app/models/gonflable.rb
@@ -3,6 +3,7 @@ class Gonflable < ApplicationRecord
   has_many :users, through: :rents
   has_many :rents, dependent: :destroy
   has_one_attached :photo
+  paginates_per 20
 
   validates :name, :price, :content, presence: true
 end
